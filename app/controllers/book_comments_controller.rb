8c62e5aabef178e4fc0afc5955e58837156d2f70
mmn1081 <101250760+mmn1081@users.noreply.github.com>
2022-05-08 11:19:49 +0000

    いいねコメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,14 +4,14 @@ class BookCommentsController < ApplicationController#コメント機能部分
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     @comment.save
-    redirect_to book_path(@book.id)#showページのどの投稿に行くかを決めるために引数は必要
+    #redirect_to book_path(@book.id)#showページのどの投稿に行くかを決めるために引数は必要 非同期通信のため
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     @comment = current_user.book_comments.find_by(book_id: @book.id)
     @comment.destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])#非同期通信のため
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #redirect_to book_path(@book.id)
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)いいね非同期通信　リダイレクト先がない場合はcreate.js.erbを探す
   end
 
   def destroy
@@ -12,6 +12,6 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(book_id: @book.id)#ログインしているユーザーのいいねをユーザーのつながり
     favorite.destroy#消す
     #redirect_to book_path(@book.id)#showページのどの投稿に行くかを決めるために引数は必要
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)いいね非同期通信　リダイレクト先がない場合はdestroy.js.erbを探す
   end
 end
